8d01aa11b9bb61cef57b462ea72df7d5a5d7499c
Declan Warn <d.warn@student.unsw.edu.au>
2019-07-28 17:08:44 +1000

    feed shows unformatted post information

--- frontend/src/Feed.js
+++ frontend/src/Feed.js
@@ -8,9 +8,34 @@ const loadFeed = async (model, feed) => {
         const post = document.createElement("li");
         post.setAttribute("data-id-post", "");
 
-        post.textContent = `
-            ${title}  
-        `;
+        const author = document.createElement("span");
+        author.setAttribute("data-id-author", "");
+        author.textContent = meta.author;
+
+        const published = document.createElement("time");
+        published.textContent = meta.published;
+
+        const image = document.createElement("img");
+        //image.src = thumbnail;
+
+        const upvotes = document.createElement("span");
+        upvotes.setAttribute("data-id-upvotes", "")
+        upvotes.textContent = `${meta.upvotes.length} upvotes`;
+
+        const postTitle = document.createElement("title");
+        postTitle.setAttribute("data-id-title", "");
+        postTitle.textContent = title;
+
+        const postText = document.createElement("p");
+        postText.textContent = text;
+
+        const numComments = document.createElement("span");
+        numComments.textContent = `${meta.comments ? meta.comments.length : 0} comments`;
+
+        const subseddit = document.createElement("span");
+        subseddit.textContent = meta.subseddit;
+
+        post.append(author, published, image, upvotes, postTitle, postText, numComments, subseddit);
 
         feed.append(post);
     });
