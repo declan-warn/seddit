04b315fe43a701167168f2e2073acd6a1af23264
Declan Warn <d.warn@student.unsw.edu.au>
2019-07-28 17:26:44 +1000

    login form now parses form and sends request

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,16 +1,28 @@
-const handleSubmit = update => event => {
+const handleSubmit = ({ apiUrl }, update) => async event => {
     event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+    const response = await fetch(`http://${apiUrl}/auth/login`, {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
+
+    console.log(response);
 };
 
 export default (model, update) => {
     const form = document.createElement("form");
-    form.addEventListener("submit", handleSubmit(update));
+    form.addEventListener("submit", handleSubmit(model, update));
 
     const username = document.createElement("input");
     username.placeholder = "Username";
+    username.name = "username";
 
     const password = document.createElement("input");
     password.placeholder = "Password";
+    password.name = "password";
 
     const submit = document.createElement("button");
     submit.textContent = "Submit";
