f5ed873ad83cf748edda20f7671e45022f58aac9
Declan Warn <d.warn@student.unsw.edu.au>
2019-07-30 01:44:42 +1000

    Comment form sends comment

--- frontend/src/component/Post.js
+++ frontend/src/component/Post.js
@@ -1,6 +1,31 @@
 import { withHeader } from "../AppHeader.js";
 import FeedItem from "./FeedItem.js";
 
+const handleSubmit = (model, update) => async event => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+    console.log(data);
+
+    const response = await fetch(`http://${model.apiUrl}/dummy/post/comment?id=${model.postId}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    const json = await response.json();
+    console.log(json);
+    if (response.status === 200) {
+        //update("LOGIN_SUCCESS", json);
+    } else {
+        alert(json.message);
+    }
+};
+
 export default (model, update) => {
     const body = document.createElement("main");
 
@@ -17,14 +42,19 @@ export default (model, update) => {
             });
             body.append(comments);
         });
-    
+
     const commentText = document.createElement("textarea");
     commentText.placeholder = "Add comment...";
+    commentText.name = "comment";
 
     const commentSubmit = document.createElement("button");
     commentSubmit.textContent = "Submit";
 
-    body.append(commentText, commentSubmit);
+    const form = document.createElement("form");
+    form.addEventListener("submit", handleSubmit(model, update));
+    form.append(commentText, commentSubmit);
+
+    body.append(form);
 
     return withHeader(model, update, body);
 };
