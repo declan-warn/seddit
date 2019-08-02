90193b7384b89638f6d82633c80be6b2d52b7cbb
Declan Warn <d.warn@student.unsw.edu.au>
2019-08-02 16:22:40 +1000

    Add more information to user profiles

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -4,9 +4,32 @@ import { createElement } from "/src/util.js";
 
 export default (model, update) => {
     const info = createElement("main", {
+        class: "profile",
         children: [
-            ["h2", {
-                children: model.routeData.username     
+            ["section", {
+                "data-id-profile-info": "",
+                class: "info",
+                children: [
+                    ["div", {
+                        "data-id-profile-picture": "",
+                    }],
+                    ["div", {
+                        children: [
+                            ["h2", {
+                                "data-id-profile-username": "",
+                                children: model.routeData.username     
+                            }],
+                            ["span", {
+                                "data-id-profile-email": "",
+                                children: model.routeData.email
+                            }],
+                            ["button", {
+                                "data-id-profile-follow": "",
+                                children: "Follow"
+                            }]
+                        ]
+                    }]
+                ]
             }]
         ]
     });
