ff95c93d7f0520005361299e5de1c15738a1e74b
Declan Warn <d.warn@student.unsw.edu.au>
2019-08-05 15:53:38 +1000

    Small progress on worker

--- frontend/src/worker.js
+++ frontend/src/worker.js
@@ -1,9 +1,44 @@
-(async function() {
+const model = {
+    apiUrl: null,
+    token: null,
+    api: null,
+};
+
+this.addEventListener(
+    "message",
+    ({ data: [msg, payload = {}] }) => {
+        switch (msg) {
+            case "SET_API_URL":
+                model.apiUrl = payload;
+                break;
+
+            case "UPDATE_TOKEN":
+                token = payload;
+                if (model.apiUrl && token !== null) {
+                    model.api = new APIWrapper(model, model.apiUrl);
+                } else {
+                    model.api = null; 
+                }
+                
+                break;
+
+            default:
+                throw new Error(`Unknown msg '${msg}'.`);
+        }
+    }
+);
+
+(async function () {
     if (Notification.permission === "default") {
         await Notification.requestPermission();
     } else if (Notification.permission === "denied") {
         alert("Notifications blocked. Please enable them.");
     }
 
-    
+    setInterval(async () => {
+        const api = model.api;
+        if (api === null) return;
+        console.log(await api.user.getFeed());
+    }, 2000);
+
 })();
