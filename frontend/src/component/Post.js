5cf247f31540892b3e2d697680c7905e4ce9a615
Declan Warn <declan.warn@gmail.com>
2019-08-13 02:55:32 +1000

    Make Post component use this instead of passed args

--- frontend/src/component/Post.js
+++ frontend/src/component/Post.js
@@ -1,68 +1,71 @@
 import { withHeader } from "/src/component/AppHeader.js";
-import FeedItem from "/src/component/FeedItem.js";
 import { createElement, toRelativeTime } from "/src/util.js";
 
-const handleSubmit = (model, update) => async event => {
-    event.preventDefault();
+import FeedItem from "/src/component/FeedItem.js";
 
-    const formData = new FormData(event.currentTarget);
-    const body = Object.fromEntries(formData.entries());
+export default function Post() {
+    const onSubmit = async event => {
+        event.preventDefault();
 
-    update("COMMENT_SUBMIT", { id: model.routeData.id, body });
-};
+        const formData = new FormData(event.currentTarget);
+        const body = Object.fromEntries(formData.entries());
 
-export default (model, update) => withHeader(model, update, createElement(
-    "main", {
-        class: "post",
-        children: [
-            ["ul", {
-                children: [
-                    FeedItem(model, update, model.routeData)
-                ]
-            }],
-            ["form", {
-                onSubmit: handleSubmit(model, update),
-                children: [
-                    ["h2", {
-                        children: "Add comment"
-                    }],
-                    ["textarea", {
-                        placeholder: "Comment...",
-                        name: "comment"
-                    }],
-                    ["button", {
-                        children: "Submit"
-                    }]
-                ]
-            }],
-            ["h2", {
-                children: "Comments"
-            }],
-            ["ul", {
-                class: "comments",
-                children: model.routeData.comments.map(
-                    ({ author, published, comment }) => createElement(
-                        "li", {
-                            class: "comment",
-                            children: [
-                                ["div", {
-                                    children: [
-                                        ["a", {
-                                            children: author
-                                        }],
-                                        ["time", {
-                                            children: toRelativeTime(published * 1000)
-                                        }]
-                                    ]
-                                }],
-                                ["span", {
-                                    children: comment
-                                }]
-                            ]
-                        }
+        this.update("COMMENT_SUBMIT", { id: this.model.routeData.id, body });
+    };
+
+    return withHeader(this.model, this.update, createElement(
+        "main", {
+            class: "post",
+            children: [
+                ["ul", {
+                    children: [
+                        FeedItem(this.model, this.update, this.model.routeData)
+                    ]
+                }],
+                ["form", {
+                    onSubmit,
+                    children: [
+                        ["h2", {
+                            children: "Add comment"
+                        }],
+                        ["textarea", {
+                            placeholder: "Comment...",
+                            name: "comment"
+                        }],
+                        ["button", {
+                            children: "Submit"
+                        }]
+                    ]
+                }],
+                ["h2", {
+                    children: "Comments"
+                }],
+                ["ul", {
+                    class: "comments",
+                    children: this.model.routeData.comments.map(
+                        ({ author, published, comment }) => createElement(
+                            "li", {
+                                class: "comment",
+                                children: [
+                                    ["div", {
+                                        children: [
+                                            ["a", {
+                                                children: author
+                                            }],
+                                            ["time", {
+                                                children: toRelativeTime(published * 1000)
+                                            }]
+                                        ]
+                                    }],
+                                    ["span", {
+                                        children: comment
+                                    }]
+                                ]
+                            }
+                        )
                     )
-                )
-            }]
-        ]
-    }
-));
+                }]
+            ]
+        }
+    ));
+}
